da3a9e4ecdb1cb10a78f3f79ecd258fd86035509
JhonSanz <ingjhonsanz@gmail.com>
2023-10-25 15:56:38 -0500

    fixed api gateway body

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,21 @@
-
+import json
 import strawberry
 from gql_strawberry.query import Query
 from gql_strawberry.mutations import Mutation
 
 
 def handler(event, context):
+    body = json.loads(event["body"])
+    if not body.get("query"):
+        return {
+            'statusCode': 400,
+            'body': {
+                "msg": "Bad body bro"
+            }
+        }
+
     schema = strawberry.Schema(query=Query, mutation=Mutation)
-    result = schema.execute_sync(event['query'])
+    result = schema.execute_sync(body['query'])
     return {
         'statusCode': 200,
         'body': {

c3a9a04b1a82c9c8b4ceb222373a2fd546879fa3
JhonSanz <ingjhonsanz@gmail.com>
2023-10-28 11:07:08 -0500

    works but too large

--- lambda_zip_generator.py
+++ lambda_zip_generator.py
@@ -1,10 +1,10 @@
 import os
 
 def install_dependencies(folder):
-    os.system(f"copy {folder} {folder}_copy -r")
-    # os.system(f"pip install -r requirements.txt -t {folder}_copy")
-    # os.system(f"powershell Compress-Archive -Path {folder}_copy/* -CompressionLevel Optimal -DestinationPath {folder}.zip")
-    # os.system(f"rmdir /s /q {folder}_copy -y")
+    os.system(f"xcopy {folder} {folder}_copy\ /E /H /q")
+    os.system(f"pip install -r requirements.txt -t {folder}_copy")
+    os.system(f"powershell Compress-Archive -Path {folder}_copy/* -CompressionLevel Optimal -DestinationPath {folder}.zip")
+    os.system(f"rmdir /s /q {folder}_copy -y")
 
 
 install_dependencies("money")

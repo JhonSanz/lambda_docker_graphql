c94c20ee6abb0f81ece437ce55535c8c9514fc97
JhonSanz <ingjhonsanz@gmail.com>
2023-10-29 12:36:16 -0500

    Using linux cli

--- lambda_zip_generator.py
+++ lambda_zip_generator.py
@@ -1,15 +1,18 @@
 import os
 
 def install_dependencies(folder):
-    os.system(f"xcopy {folder} {folder}_copy\ /E /H /q")
-    os.system(f"xcopy utils.py {folder}_copy /F /H /q")
+    print(f"Copy files for {folder}")
+    os.system(f"cp -r {folder} {folder}_copy")
+    os.system(f"cp utils.py {folder}_copy")
+    os.system(f"rm -rf {folder}_copy/__pycache__")
+    print(f"Install requirements for {folder}")
     os.system(f"pip install -r requirements.txt -t {folder}_copy")
-    # os.system(f"powershell Compress-Archive -Path {folder}_copy/* -CompressionLevel Optimal -DestinationPath {folder}.zip")
-    # os.system(f"rmdir /s /q {folder}_copy -y")
+    print(f"Generate zip file for {folder}")
+    os.system(f"cd {folder}_copy && zip -r ../{folder}.zip .")
+    os.system(f"rm -rf {folder}_copy")
 
-# for item in [
-#     "account", "asset", "broker",
-#     "deposit", "money", "position"
-# ]:
-#     install_dependencies(item)
-install_dependencies("broker")
+for item in [
+    "account", "asset", "broker",
+    "deposit", "money", "position"
+]:
+    install_dependencies(item)

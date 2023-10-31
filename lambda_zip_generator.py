91ecf1982c6e0999d8198fee4f8248666125b865
JhonSanz <ingjhonsanz@gmail.com>
2023-10-31 13:34:24 -0500

    added options cdk

--- lambda_zip_generator.py
+++ lambda_zip_generator.py
@@ -3,18 +3,18 @@ import os
 def install_dependencies(folder):
     print(f"Copy files for {folder}")
     os.system(f"cp -r {folder} {folder}_copy")
-    os.system(f"cp resources.utils.py {folder}_copy")
-    os.system(f"cp resources.comparison.py {folder}_copy")
-    os.system(f"cp resources.filters.py {folder}_copy")
+    # os.system(f"cp resources.utils.py {folder}_copy")
+    # os.system(f"cp resources.comparison.py {folder}_copy")
+    # os.system(f"cp resources.filters.py {folder}_copy")
     os.system(f"rm -rf {folder}_copy/__pycache__")
     print(f"Install requirements for {folder}")
     os.system(f"pip install -r requirements.txt -t {folder}_copy")
     print(f"Generate zip file for {folder}")
-    os.system(f"cd {folder}_copy && zip -r ../positions_backend_micro/data/{folder}.zip .")
+    os.system(f"cd {folder}_copy && zip -r ../../positions_backend_micro/{folder}.zip .")
     os.system(f"rm -rf {folder}_copy")
 
 for item in [
     "account", "asset", "broker",
     "deposit", "money", "position"
 ]:
-    install_dependencies("endpoints" + item)
+    install_dependencies("endpoints/" + item)
--- positions_backend_micro/positions_backend_micro/positions_backend_micro_stack.py
+++ positions_backend_micro/positions_backend_micro/positions_backend_micro_stack.py
@@ -16,30 +16,35 @@ class PositionsBackendMicroStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         http_api = apigwv2.HttpApi(self, "positions-backend-micro")
 
-        endpoint = "broker"
+        for endpoint in [
+            "account", "asset", "broker",
+            "deposit", "money", "position"
+        ]:
+            lambda_function = lambda_.Function(
+                self, f"{endpoint}EndpointLambda",
+                handler="lambda_function.lambda_handler",
+                runtime=lambda_.Runtime.PYTHON_3_11,
+                code=lambda_.Code.from_asset(f"endpoints/{endpoint}.zip"),
+                description=f"handles {endpoint} endpoint.",
+                function_name=f"{endpoint}EndpointLambda"
+            )
+            lambda_function.add_to_role_policy(iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["dynamodb:*"],
+                resources=['*'],
+            ))
 
-        lambda_function = lambda_.Function(
-            self, f"{endpoint}EndpointLambda",
-            handler="lambda_function.lambda_handler",
-            runtime=lambda_.Runtime.PYTHON_3_11,
-            code=lambda_.Code.from_asset(f"data/{endpoint}.zip"),
-        )
-        lambda_function.add_to_role_policy(iam.PolicyStatement(
-            effect=iam.Effect.ALLOW,
-            actions=["dynamodb:*"],
-            resources=['*'],
-        ))
+            books_integration = HttpLambdaIntegration(f"{endpoint}Integration", lambda_function)
+            http_api.add_routes(
+                path=f"/{endpoint}",
+                methods=[apigwv2.HttpMethod.ANY],
+                integration=books_integration
+            )
 
-        books_integration = HttpLambdaIntegration(f"{endpoint}Integration", lambda_function)
-        http_api.add_routes(
-            path=f"/{endpoint}",
-            methods=[apigwv2.HttpMethod.ANY],
-            integration=books_integration
-        )
-
-        dynamodb.Table(
-            self,
-            id='dynamodbTable',
-            table_name=endpoint,
-            partition_key=dynamodb.Attribute(name='id', type=dynamodb.AttributeType.STRING)
-        )
+            dynamodb.Table(
+                self,
+                id=f'{endpoint}_dynamodbTable',
+                table_name=endpoint,
+                partition_key=dynamodb.Attribute(name='id', type=dynamodb.AttributeType.STRING),
+                billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            )
